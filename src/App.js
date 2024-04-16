edc03e9fa8e38f3f73089db7ab7c82d3b734ec97
siri-project <wdt0134@gmail.com>
2024-04-16 10:24:09 +0900

    all file commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"; // react 라이브러리 호출
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => { // App 이라는 이름의 상수 선언, 화살표 함수(arrow function)로 정의된 함수형 컴포넌트는 입력을 받아서 화면에 렌더링할 JSX요소를 반환하는 역할.
+	return <div>App</div>; // 함수형 컴포넌트에서 반환하는 JSX(Javascript XML)요소
+};
 
-export default App;
+export default App; // App 컴포넌트를 다른 파일에서 재사용할 수 있도록 내보내기 설정
--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root")); // creatRoot() 메서드를 사용하여 렌더링할 최상위 컨테이너 생성
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
