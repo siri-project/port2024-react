65f728f2c8dc108637d1f6effc1b7acc7e0189de
siri-project <wdt0134@gmail.com>
2024-04-16 17:33:56 +0900

    header commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+
+const headerNav = [
+	{
+		title: "intro",
+		url: "#intro"
+	},
+	{
+		title: "skill",
+		url: "#skill"
+	},
+	{
+		title: "site",
+		url: "#site"
+	},
+	{
+		title: "portfolio",
+		url: "#port"
+	},
+	{
+		title: "contact",
+		url: "#contact"
+	},
+];
 
 const Header = () => {
-	return <div>Header</div>;
+	const [show, setShow] = useState(false);
+
+	const toggleMenu = () => {
+		setShow((prevShow) => !prevShow);
+	};
+
+	return (
+		<header id="header" role="banner">
+			<div className="header-inner">
+				<div className="header-logo">
+					<a href="/">
+						portfolio
+						<em>react.js</em>
+					</a>
+				</div>
+				<nav className={`header-nav ${show ? "show" : ""}`} role="navigation" aria-label="메인 메뉴">
+					<ul>
+						{headerNav.map((nav, key) =>(
+							<li key={key}>
+								<a href={nav.url}>{nav.title}</a>
+							</li>
+						))}
+					</ul>
+				</nav>
+				<div className="header-nav-mobile" id="headerToggle" aria-controls="primary-menu" aria-expanded={show ? "true" : "false"} role="button" tabIndex="0" onClick={toggleMenu}>
+					<span></span>
+				</div>
+			</div>
+		</header>
+	);
 };
 
 export default Header;
