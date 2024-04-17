f05fe9707d0349b7d6038a15e3abbff75c277ec5
siri-project <wdt0134@gmail.com>
2024-04-17 10:26:19 +0900

    intro commit

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
+import about from "../assets/img/about.jpg"
+
+const introText = {
+	title: 'portfolio developer',
+	desc: ['My', 'portfolio'],
+};
+
 const Intro = () => {
-	return <div>Intro</div>;
+	return (
+		<section id="intro">
+			<div className="intro-inner">
+				<div className="intro-title">{introText.title}</div>
+				<div className="intro-text">
+					<div className="text">
+						<div>{introText.desc[0]}</div>
+						<div>{introText.desc[1]}</div>
+					</div>
+					<div className="img">
+						<img src={about} alt="about" />
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default Intro;
