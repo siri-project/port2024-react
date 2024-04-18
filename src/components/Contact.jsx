d98bbfcc7e9c58d01cdad1d1ee8e3a6cdd962a7e
siri-project <wdt0134@gmail.com>
2024-04-18 17:51:09 +0900

    contact commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,46 @@
 import React from "react";
 
+const contactText = [
+	{
+		link: "mailto:wdt0134@gmail.com",
+		title: "mail"
+	},
+	{
+		link: "https://open.kakao.com/o/sFPNcOmg" ,
+		title: "kakao"
+	},
+	{
+		link: "https://line.me/ti/p/RVWCnGJVlM" ,
+		title: "line"
+	},
+	// {
+	// 	link: "tel:010-8644-4534",
+	// 	title: "phone"
+	// },
+];
+
 const Contact = () => {
-	return <div>Contact</div>;
+	return (
+		<section id="contact">
+			<div className="contact-inner">
+				<h2 className="contact-title">
+					Contact
+					<em>연락은 메일로 부탁드립니다.</em>
+				</h2>
+				<div className="contact-text">
+					<div className="text">
+						{contactText.map((contact, key) => (
+							<div key={key}>
+								<a href={contact.link} target="_blank" rel="noreferrer">
+									{contact.title}
+								</a>
+							</div>
+						))}
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default Contact;
