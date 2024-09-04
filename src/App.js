ef9a27fca72ba27e9f3dcf927de663fa98741df5
siri <wdt0134@gmail.com>
2024-09-04 21:52:40 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 import HomeView from "./views/HomeView";
 // import smooth from "./utils/smooth";
@@ -11,11 +11,11 @@ const App = () => {
 	// }, []);
 
 	return (
-		<BrowserRouter basename={process.env.PUBLIC_URL}>
+		//<BrowserRouter basename={process.env.PUBLIC_URL}>
 			<Routes>
 				<Route path="/" element={<HomeView />} />
 			</Routes>
-		</BrowserRouter>
+		//</BrowserRouter>
 	);
 };
 
